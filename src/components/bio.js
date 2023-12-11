711f46acbebbc88d10ffd507986113cca435e871
gangadhar <gangadhar@gangadhars-MacBook-Pro.local>
2023-12-11 21:34:24 +0100

    Updates twitter text

--- src/components/bio.js
+++ src/components/bio.js
@@ -32,22 +32,12 @@ const Bio = () => {
 
   return (
     <div className="bio">
-      {/* <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      /> */}
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            Follow us on Twitter
+            Follow me on Twitter
           </a>
         </p>
       )}
